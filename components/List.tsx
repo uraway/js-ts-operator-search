b2dd183f9a8b7e155cb24515f19efd0ca28b4079
Masato Urai <masato.uraway@gmail.com>
2020-11-15 17:36:52 +0900

    use Transition

--- components/List.tsx
+++ components/List.tsx
@@ -1,8 +1,9 @@
 import { NextComponentType, NextPageContext } from "next";
 import React from "react";
-import { Button, RenderIf } from "react-rainbow-components";
+import { Button } from "react-rainbow-components";
 import styled from "styled-components";
 import { Data } from "../types/data";
+import { Transition } from "react-transition-group";
 
 type Props = {
   value: Data;
@@ -17,6 +18,7 @@ const Container = styled.div`
 const StyledButton = styled(Button)`
   min-width: 20px;
   margin: 2px;
+  opacity: ${({ state }) => (state === "exited" ? 1 : 0)};
 `;
 
 export const List: NextComponentType<
@@ -25,17 +27,23 @@ export const List: NextComponentType<
   Props
 > = React.memo(({ value, options, onClick }) => {
   return (
-    <RenderIf isTrue={!value}>
-      <Container className="rainbow-p-vertical_large rainbow-align-content_center rainbow-flex_wrap">
-        {options.map((option) => (
-          <StyledButton
-            key={option.description.toString()}
-            label={option.label}
-            onClick={() => onClick(option)}
-          />
-        ))}
-      </Container>
-    </RenderIf>
+    <Container className="rainbow-p-vertical_large rainbow-align-content_center rainbow-flex_wrap">
+      {options.map((option, index) => (
+        <Transition
+          key={option.description.toString()}
+          in={!!value}
+          timeout={20 * index}
+        >
+          {(state) => (
+            <StyledButton
+              state={state}
+              label={option.label}
+              onClick={() => onClick(option)}
+            />
+          )}
+        </Transition>
+      ))}
+    </Container>
   );
 });
 
