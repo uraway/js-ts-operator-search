e8f7d5ead62a32c57334523df2c0e0ed8e1ee1ae
Masato Urai <masato.uraway@gmail.com>
2022-02-01 12:37:50 +0900

    Remove react rainbow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import { Data } from '../types/data'
 import { Description, Layout, List } from '../components'
 import { getAbsoluteUrl } from '../utils'
-import { Heading, SlideFade } from '@chakra-ui/react'
-import { Lookup } from 'react-rainbow-components'
+import {
+  Heading,
+  SlideFade,
+  Input,
+  InputGroup,
+  IconButton,
+  InputRightElement,
+  InputLeftElement,
+} from '@chakra-ui/react'
 import { NextPage } from 'next'
 import fetch from 'isomorphic-fetch'
-import React, { useCallback, useState } from 'react'
-import { LookupValue } from 'react-rainbow-components/components/types'
+import React, { ChangeEventHandler, useCallback, useState } from 'react'
+import { CloseIcon, SearchIcon } from '@chakra-ui/icons'
 
 type Props = {
   data: Data[]
@@ -14,7 +21,7 @@ type Props = {
 
 const filter = (query: string, os: Data[]) => {
   if (query) {
-    return os.filter((item) => item?.label?.toString().includes(query))
+    return os.filter((item) => item.label.includes(query))
   }
   return []
 }
@@ -22,36 +29,27 @@ const filter = (query: string, os: Data[]) => {
 const Home: NextPage<Props> = ({ data }) => {
   const [value, setValue] = useState<Data | undefined>()
   const [inputValue, setInputValue] = useState('')
-  const [options, setOptions] = useState<Data[] | undefined>()
+  const [options, setOptions] = useState<Data[] | undefined>(data)
 
-  const onSearch = (newInputValue: string) => {
-    if (!!options && newInputValue?.length > inputValue?.length) {
-      setInputValue(newInputValue)
-      setOptions(filter(newInputValue, options))
-    } else if (newInputValue) {
-      setInputValue(newInputValue)
-      setOptions(filter(newInputValue, data))
+  const onInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setInputValue(e.target.value)
+    if (e.target.value.length > 0) {
+      setOptions(filter(e.target.value, data))
     } else {
-      setInputValue(inputValue)
-      setOptions(undefined)
+      setValue(undefined)
+      setOptions(data)
     }
   }
 
-  const onChange = useCallback(
-    (value: LookupValue | null) => {
-      if (value) {
-        const selectedOption = data?.find((o) => o.label === value.label)
-        setValue(selectedOption)
-      } else {
-        setValue(undefined)
-      }
-    },
-    [data]
-  )
+  const onChange = useCallback((value: Data) => {
+    setValue(value)
+    setInputValue(value.label)
+  }, [])
 
   const onClear = () => {
+    setInputValue('')
     setValue(undefined)
-    setOptions(undefined)
+    setOptions(data)
   }
 
   return (
@@ -65,16 +63,23 @@ const Home: NextPage<Props> = ({ data }) => {
       >
         JavaScript/TypeScript 演算子検索
       </Heading>
-      <Lookup
-        id="lookup"
-        options={options}
-        value={value}
-        onChange={onChange}
-        onSearch={onSearch}
-        onClick={onClear}
-      />
+      <InputGroup>
+        <InputLeftElement>
+          <SearchIcon />
+        </InputLeftElement>
+        <Input id="lookup" value={inputValue} onChange={onInputChange} />
+        <InputRightElement>
+          <IconButton
+            variant="ghost"
+            size="sm"
+            onClick={onClear}
+            aria-label="Clear input value"
+            icon={<CloseIcon />}
+          />
+        </InputRightElement>
+      </InputGroup>
       <SlideFade in={!value} unmountOnExit offsetY="100px">
-        <List options={data} onClick={onChange} />
+        <List options={options} onClick={onChange} />
       </SlideFade>
       <SlideFade in={!!value} unmountOnExit offsetY="100px">
         <Description value={value} />
